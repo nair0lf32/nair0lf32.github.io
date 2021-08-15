da504262c34cab395080e80f29bad7a88c27b35e
nair0lf32 <florianedem@gmail.com>
2021-08-15 05:04:13 +0100

    added a preloader

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.2 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Freelancer v7.0.2 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
 //
+
+//
+
 // Scripts
 // 
-
 window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
     var navbarShrink = function () {
@@ -42,7 +44,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    
+
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
@@ -52,3 +54,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+//loading hide function
+function preloaderFadeOutInit(){
+    $('.loader').fadeOut('slow');
+    $('body').attr('id','');
+    $(' html, body').css({overflow: 'auto'});
+    }
+    // Window load function
+    jQuery(window).on('load', function () {
+    (function ($) {
+    preloaderFadeOutInit();
+    })(jQuery);
+    });
