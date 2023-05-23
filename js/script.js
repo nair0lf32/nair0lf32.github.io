23fe5bcaac4ae8eb07ec951fb625258198ee40c7
nair0lf32 <florianedem@gmail.com>
2023-05-23 21:32:22 +0100

    add dark mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-onload.alert(0);
+const toggler = document.getElementById("switch");
+toggler.addEventListener("click", switchmode);
+
+function switchmode() {
+    const bg = document.getElementById("light");
+    const toggler = document.getElementById("switch");
+
+    bg.classList.toggle("dark");
+    if (bg.classList.contains("dark")) {
+        toggler.innerHTML = "Light Mode"
+    } else {
+        toggler.innerHTML = "Dark Mode"
+    }
+}
